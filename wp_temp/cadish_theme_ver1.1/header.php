78409c28de6d61d41696727183f13deaf869d623
saeka <saeka@cadish.co.jp>
2017-03-24 17:47:41 +0900

    検索結果ページ、404ページでエラーが出ていたので再修正

--- wp_temp/cadish_theme_ver1.1/header.php
+++ wp_temp/cadish_theme_ver1.1/header.php
@@ -62,11 +62,9 @@ if (is_single()){
 	echo '<meta property="og:url" content="'; echo $canonical_url; echo '">';echo "\n";
 	echo '<meta property="og:type" content="blog">';echo "\n";
 }
-if( !(is_home()) and !( is_search()) and !( is_404()) ) {
+if (is_single()){
 	$str = $post->post_content;
 	$searchPattern = '/<img.*?src=(["\'])(.+?)\1.*?>/i';
-}
-if (is_single()){
 	if (has_post_thumbnail()){
 		$image_id = get_post_thumbnail_id();
 		$image = wp_get_attachment_image_src( $image_id, 'full');
