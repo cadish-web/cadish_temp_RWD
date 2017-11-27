9637de30f0869a98fc513b6c74b4d84a6ff67518
ayano <ayano@cadish.co.jp>
2017-11-27 16:33:59 +0900

    [js_sample/jquery.tab] 複数設置できるように修正

--- js_sample/jquery.tab/common/js/jquery.tab.js
+++ js_sample/jquery.tab/common/js/jquery.tab.js
@@ -1,16 +1,15 @@
 $(function() {
-	$('#tabs a[href^="#panel"]').click(function(){
-		$("#tabs .panel").hide();
+	$('.tabs a[href^="#panel"]').click(function(){
+		$(this).parents('.tabs').children('.panel').hide();
 
 		// class名の付け外し
-		$("#tabs a").removeClass("active");
+		$(this).parent('li').siblings().find('a').removeClass('active');
 		$(this).addClass("active");
 
 		$(this.hash).fadeIn();
 		return false;
 	});
 	//わざと1つ目を表示させておくことができます
-	//$('#tabs a[href^="#panel"]:eq(0)').trigger('click');
-	$("#tabs .panel").hide();
-	$("#tabs #panel1").show();
+	$('.tabs .panel').hide();
+	$('.tabs .panel:first-of-type').show();
 })
--- js_sample/jquery.tab/common/js/jquery.tab2.js
+++ js_sample/jquery.tab/common/js/jquery.tab2.js
@@ -1,28 +1,25 @@
 $(function() {
-	$('#tabs a[href^="#panel"]').click(function(){
-		$("#tabs .panel").hide();
+	$('.tabs a[href^="#panel"]').click(function(){
+		$(this).parents('.tabs').children('.panel').hide();
 
 		// class名の付け外し
-		$("#tabs a").removeClass("active");
+		$(this).parent('li').siblings().find('a').removeClass('active');
 		$(this).addClass("active");
 
 		// imgの _on, _off も一緒に切り替えたい場合
-		var thisImg = $(this).children('img');
-		var thisPath = thisImg.attr('src').replace('_off', '_on');
-		thisImg.attr('src', thisPath);
-
-		var arr = $(this).parent('li').siblings();
-		arr.each(function(i, val) {
-			var valImg = $(val).find('img');
-			var valPath = valImg.attr('src').replace('_on', '_off');
-			valImg.attr('src', valPath);
+		$(this).parent('li').siblings().each(function(){
+			$(this).find('img').attr('src', function(){
+				return $(this).attr('src').replace('_on', '_off');
+			});
+		});
+		$(this).children('img').attr('src', function(){
+			return $(this).attr('src').replace('_off', '_on');
 		});
 
 		$(this.hash).fadeIn();
 		return false;
 	});
 	//わざと1つ目を表示させておくことができます
-	//$('#tabs a[href^="#panel"]:eq(0)').trigger('click');
-	$("#tabs .panel").hide();
-	$("#tabs #panel1").show();
+	$(".tabs .panel").hide();
+	$(".tabs .panel:first-of-type").show();
 })
